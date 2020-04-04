0f864193fb6acf14865b903b88dc24b606aa80d9
Allister <allisterkmoon@gmail.com>
2020-04-04 16:57:10 -0700

    update express stuff to display app correctly

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const history = require('connect-history-api-fallback')
 const path = require('path')
-
-require('dotenv').config({
-  path: path.resolve(__dirname, '.env'),
-})
+require('dotenv').config()
 
 const { PORT, HOST } = require('./config/server')
 const app = express()
 
-app.use('/static', express.static(path.join(__dirname, 'dist', 'static')))
+app.use(express.static(path.join(__dirname, 'dist')))
 app.use(history())
+app.use(express.static(path.join(__dirname, 'dist')))
+app.get('/', (req, res) => res.render(path.join(__dirname, 'dist/index.html')))
 
 app.listen(PORT, HOST)
+console.log(`started factory-town-production-calculator server. Listening on: ${PORT}:${HOST}`)
