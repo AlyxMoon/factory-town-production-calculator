e0c5e325c9dc9b267cc559460a58833f274e8301
Allister <allisterkmoon@gmail.com>
2020-04-04 21:20:35 -0700

    add pagination for the recipe table

--- src/components/RecipeList.vue
+++ src/components/RecipeList.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="recipe-list-component">
     <h2>Recipes</h2>
+    <div class="pagination-wrapper">
+      <span class="page-count">Showing page {{ page * 10 }}-{{ (page + 1) * 10 }} of {{ recipes.length }} recipes</span>
+      <button v-if="hasPrevPage" @click="page--">previous</button>
+      <button v-if="hasNextPage" @click="page++">next</button>
+    </div>
     <table>
       <thead>
         <tr>
@@ -12,7 +17,7 @@
         </tr>
       </thead>
       <tbody>
-        <template v-for="(recipe, i) in recipes">
+        <template v-for="(recipe, i) in recipesOfPage">
           <recipe-list-item
             class="border-t"
             :key="'recipe-item-' + i + '-' + recipe.name"
@@ -38,6 +43,11 @@
         </template>
       </tbody>
     </table>
+    <div class="pagination-wrapper">
+      <span class="page-count">Showing page {{ page + 1 }} of {{ recipes.length }} recipes</span>
+      <button v-if="hasPrevPage" @click="page--">previous</button>
+      <button v-if="hasNextPage" @click="page++">next</button>
+    </div>
   </div>
 </template>
 
@@ -56,10 +66,22 @@ export default {
   data () {
     return {
       recipes: [],
+      page: 0,
     }
   },
   async mounted () {
     this.recipes = await this.fetchRecipes()
+  },
+  computed: {
+    hasPrevPage () {
+      return this.page > 0
+    },
+    hasNextPage () {
+      return (this.page + 1) * 10 < this.recipes.length
+    },
+    recipesOfPage () {
+      return this.recipes.slice(this.page * 10, (this.page + 1) * 10)
+    },
   },
   methods: {
     ...mapActions(['fetchRecipes']),
@@ -72,5 +94,14 @@ table {
   width: 100%;
   border: 1px solid black;
   border-collapse: collapse;
+}
+
+.pagination-wrapper {
+  display: flex;
+  flex-direction: row;
+
+  & > *:not(:last-child)  {
+    margin-right: 10px;
+  }
 }
 </style>
